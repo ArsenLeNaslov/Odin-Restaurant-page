28cdcb314ec275a8eede5f302ddb870c503cde72
Arsén <rhos.andrea@gmail.com>
2025-11-21 16:51:46 +0100

    Bundle `./src/index.js` and Serve `./src/template.html` in dev

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// Webpack config: bundles `./src/index.js` and serves `./src/template.html` in dev
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+
+  devtool: "eval-source-map",
+
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/template.html"],
+    open: true,
+    hot: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: "index.html"
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/i,
+        use: "html-loader"
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]"
+        }
+      }
+    ]
+  }
+};
